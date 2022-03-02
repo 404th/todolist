9f0bfcac28192a86e10dc062ac3e62d6a0137553
404th <umarov.doniyor.2002@gmail.com>
2022-03-02 23:34:03 +0500

    Some changes

--- cmd/main.go
+++ cmd/main.go
@@ -7,28 +7,35 @@ import (
 	"github.com/404th/todolist/pkg/handler"
 	"github.com/404th/todolist/pkg/repository"
 	"github.com/404th/todolist/pkg/service"
-	"github.com/spf13/viper"
+	_ "github.com/lib/pq"
 )
 
 func main() {
 	// init Config
-	if err := initConfig(); err != nil {
-		log.Fatalf("Error while initializing Config file: %s", err.Error())
+	// if err := initConfig(); err != nil {
+	// 	log.Fatalf("Error while initializing Config file: %s", err.Error())
+	// }
+
+	// repos := repository.NewRepository()
+
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     "localhost",
+		Port:     "5555",
+		Username: "postgres",
+		DBName:   "simple_bank",
+		Password: "pass",
+		SSLMode:  "disable",
+	})
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	repos := repository.NewRepository()
+	repos := repository.NewRepository(db)
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 	// initialize Server
 	srv := new(todolist.Server)
-	if err := srv.Run(viper.GetString("8000"), handlers.InitRoutes()); err != nil {
+	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
 		log.Fatalf("Error while runnig http server: %s", err.Error())
 	}
-}
-
-// init config/viper file
-func initConfig() error {
-	viper.AddConfigPath("configs")
-	viper.SetConfigName("config")
-	return viper.ReadInConfig()
 }
--- pkg/handler/auth.go
+++ pkg/handler/auth.go
@@ -1,6 +1,19 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
 
-func (h *Handler) signUp(c *gin.Context) {}
+	"github.com/404th/todolist"
+	"github.com/gin-gonic/gin"
+)
+
+func (h *Handler) signUp(c *gin.Context) {
+	var input todolist.User
+
+	if err := c.BindJSON(&input); err != nil {
+		newErrorResponse(c, http.StatusBadRequest, err.Error())
+		return
+	}
+}
+
 func (h *Handler) signIn(c *gin.Context) {}
